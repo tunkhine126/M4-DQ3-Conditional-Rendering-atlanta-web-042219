1d96911eea421e9cc9845f03019c7b238cdf24d3
tunkhine126 <tunkhine126@gmail.com>
2019-06-27 12:36:43 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const MenuBar = (props) => {
 
+
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -15,19 +16,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={props.handleClick} className="item active" id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={props.handleClick} className="item" id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={props.handleClick} className="item" id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={props.handleClick} className="item" id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,6 +4,25 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+    constructor () {
+      super();
+      this.state = {
+        card: ''
+      }
+    }
+
+    handleClick = (e) => {
+      // console.log(e.target.id)
+        if(e.target.id === 'profile') {
+        this.setState({card: <Profile/>})
+      } else if(e.target.id === 'photo') {
+          this.setState({card: <Photos/>})
+      } else if(e.target.id === 'cocktail') {
+          this.setState({card: <Cocktails/>})
+      } else if(e.target.id === 'pokemon') {
+          this.setState({card: <Pokemon/>})
+        }
+    }
 
   render() {
 
@@ -13,11 +32,11 @@ class MainBox extends React.Component {
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    const detailsToDisplay = this.state.card
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar handleClick={this.handleClick} />
         {detailsToDisplay}
       </div>
     )
